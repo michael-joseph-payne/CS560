a0d48299598a5e00099182f1f671d991539ca0c8
Chris Rich <chris@Chriss-MacBook-Pro.local>
2013-02-27 09:50:32 -0800

    Inital Commit for OpenCaption

---
+++ gen/com/psu/hci/OpenCaption/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.psu.hci.OpenCaption;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ gen/com/psu/hci/OpenCaption/R.java
@@ -0,0 +1,5 @@
+package com.psu.hci.OpenCaption;
+
+/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
+public final class R {
+}
